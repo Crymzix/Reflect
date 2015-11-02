247aca500d9a44e4edc668b1d849431c383c8292
Chris Li <chrisli@hotmail.ca>
2015-11-01 16:43:44 -0800

    Added image capture functionality for event page.

--- Reflect/app/shared/view-models/event-view-model.js
+++ Reflect/app/shared/view-models/event-view-model.js
@@ -1,9 +1,42 @@
 var observableModule = require("data/observable");
+var frameModule = require("ui/frame");
+
 var EventViewModel = (function (_super) {
     __extends(EventViewModel, _super);
-    function EventViewModel(context) {
+
+    var that;
+
+    function EventViewModel(event) {
         _super.call(this);
+        that = this;
+
+        this.set("eventCoverPhoto", event.cover_photo.url);
+        this.set("eventTitle", event.title);
+        this.set("eventDate", event.start_date);
+        this.set("eventLocation", event.location);
+        this.set("eventDescription", event.description);
     }
+
+    EventViewModel.prototype.upload = function(imageView) {
+
+        var bitmap = imageView.imageSource.android;
+
+        var outputStream = new java.io.ByteArrayOutputStream();
+        bitmap.compress(android.graphics.Bitmap.CompressFormat.PNG, 100, outputStream);
+        var image = outputStream.toByteArray();
+        var imageFile = new com.parse.ParseFile("image.png", image);
+        imageFile.saveInBackground(new com.parse.SaveCallback({
+            done: function (error) {
+
+                console.log(imageFile.getUrl());
+
+            }
+        }));
+    };
+
+    EventViewModel.prototype.makeUploadVisible = function() {
+
+    };
 
     return EventViewModel;
 })(observableModule.Observable);
