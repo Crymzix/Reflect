b8c44b2399a0aa770812013a2e1b7c0a9d5c3a9c
vDeo <vineet.deoo@gmail.com>
2015-11-19 00:35:12 -0800

    Used client implicit auth for instagram api

--- Reflect/app/views/webview/instagram-webview.js
+++ Reflect/app/views/webview/instagram-webview.js
@@ -19,6 +19,11 @@ function getCodeFromURI(uri){
     return code[1];
 }
 
+function getTokenFromUri(uri) {
+    var token = uri.split("#access_token=");
+    console.log("Code split 0:" + code[0] + " Code Split 1: " + code[1]);
+    return token[1];
+}
 
 
 exports.loaded = function(args) {
@@ -26,9 +31,9 @@ exports.loaded = function(args) {
     var web = page.getViewById("webView");
 
     web.url =instagramAPIURL + "oauth/authorize/?client_id=" + clientID +
-    "&redirect_uri=" + redirectURI + "&response_type=code";
+    "&redirect_uri=" + redirectURI + "&response_type=token";
     console.log(instagramAPIURL + "oauth/authorize/?client_id=" + clientID +
-    "&redirect_uri=" + redirectURI + "&response_type=code");
+    "&redirect_uri=" + redirectURI + "&response_type=token");
 
     // FOR SOME REASON THE THREE STEP SERVER SIDE PROCESS DOESN'T WORK
     // TODO: Use two step client side process
@@ -39,33 +44,39 @@ exports.loaded = function(args) {
             console.log(args.url);
             if(args.url.indexOf("error") === -1 && args.url.indexOf("instagram") === -1){
                 console.log(args.url);
-                var apiCode = getCodeFromURI(args.url);
-                console.log("just before fetch");
-                var result;
-                fetchModule.fetch(config.instagramAPIURL + "oauth/access_token/", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        client_id: clientID,
-                        client_secret: clientSecret,
-                        grant_type: "authorization_code",
-                        redirect_uri: redirectURI,
-                        code: apiCode
-                    })
-                })
-                    .then(handleErrors)
-                    .then(function(response) {
-                        //Setting shared preferences here
-                        //appSettings.setString("instagram_access_token", response.content.toJSON.access_token)
-                        console.log(JSON.stringify(response));
-                        return(response.content.toJSON());
-                    })
-                    .then(function(data) {
-                        //appSettings.setString("instagram_access_token", response.content.toJSON.access_token)
-                        console.log(data.access_token);
-                    });
+                var token = getTokenFromURI(args.url);
+                var navigationEntry = {
+                    moduleName: "views/main/main-page",
+                    backstackVisible: false
+                };
+                topmost.navigate(navigationEntry);
+                //var apiCode = getCodeFromURI(args.url);
+                //console.log("just before fetch");
+                //var result;
+                //fetchModule.fetch(config.instagramAPIURL + "oauth/access_token/", {
+                //    method: "POST",
+                //    headers: {
+                //        "Content-Type": "application/json"
+                //    },
+                //    body: JSON.stringify({
+                //        client_id: clientID,
+                //        client_secret: clientSecret,
+                //        grant_type: "authorization_code",
+                //        redirect_uri: redirectURI,
+                //        code: apiCode
+                //    })
+                //})
+                //    .then(handleErrors)
+                //    .then(function(response) {
+                //        //Setting shared preferences here
+                //        //appSettings.setString("instagram_access_token", response.content.toJSON.access_token)
+                //        console.log(JSON.stringify(response));
+                //        return(response.content.toJSON());
+                //    })
+                //    .then(function(data) {
+                //        //appSettings.setString("instagram_access_token", response.content.toJSON.access_token)
+                //        console.log(data.access_token);
+                //    });
             }else{
                 message = "ERROR!! ";
             }
